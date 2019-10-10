f93a2466907f28d26c9663a4cdce493a13b6c401
Alex Flores <SorAxel360@hotmail.com>
2019-10-09 21:43:53 -0400

    Restarted Project
    +Restarted Project (again :c)

---
+++ UnnamedDungeon/Source/UnnamedDungeon/UnnamedDungeonGameMode.h
@@ -0,0 +1,19 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UnnamedDungeonGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AUnnamedDungeonGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AUnnamedDungeonGameMode();
+};
+
+
+

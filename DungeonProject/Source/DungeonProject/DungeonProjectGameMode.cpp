e0e52832fbc4ec172e018865697f9f43c074581c
Angeline <siv.angeline@ymail.com>
2020-10-12 14:25:44 -0400

    Fix find default pawn to BP_Character

--- DungeonProject/Source/DungeonProject/DungeonProjectGameMode.cpp
+++ DungeonProject/Source/DungeonProject/DungeonProjectGameMode.cpp
@@ -7,7 +7,7 @@
 ADungeonProjectGameMode::ADungeonProjectGameMode()
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPersonCPP/Blueprints/ThirdPersonCharacter"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPersonCPP/Blueprints/BP_Character"));
 	if (PlayerPawnBPClass.Class != NULL)
 	{
 		DefaultPawnClass = PlayerPawnBPClass.Class;
